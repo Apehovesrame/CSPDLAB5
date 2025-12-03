7b5237d678adff88050c2523365839276dc89ce1
Apehovesrame <m.zonin@mail.ru>
2025-12-03 10:58:10 +0300

    Добавлен ввод данных и каркас приложения

--- CSPDLAB5/Program.cs
+++ CSPDLAB5/Program.cs
@@ -10,6 +10,20 @@ namespace CSPDLAB5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание 2. Расчет уравнений.");
+            Console.Write("Введите значение X: ");
+
+            if (double.TryParse(Console.ReadLine(), out double x))
+            {
+                Console.WriteLine($"Вы ввели X = {x}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено не число.");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
